9cbd947d6baa770be62348b3f96d0b8b751b57ce
Apurva Purushotama <54936708+ApurvaP-97@users.noreply.github.com>
2023-08-19 15:30:21 -0500

    Add single correct answer animation: energy pyramid slide
    Essential Knowledge: Energy Flow in Ecosystems

---
+++ BiologyAnimation/Ecology/EnergyPyramidKnowledgeCheck/js/parameters.js
@@ -0,0 +1,19 @@
+var data = [
+	{
+		"id": 1,
+		"value": "Producers"
+	},
+	{
+		"id": 2,
+		"value": "Carnivores"
+	},
+	{
+		"id": 3,
+		"value": "Herbivores"
+	},
+	{
+		"id": 4,
+		"value": "Top Predator"
+	}
+];	
+	
